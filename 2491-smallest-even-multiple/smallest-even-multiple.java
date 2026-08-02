664b501379ddd4cc917d772c5bdf9e58aafbac06
AKALYA <akalyamurugesan007@gmail.com>
2026-08-02 12:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (51.77%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public int smallestEvenMultiple(int n) 
+    {
+        int result;
+       if(n%2==0)
+       {
+        result=n;
+       }
+       else
+       {
+       result=n*2;
+       }
+       return result;
+    }
+}
