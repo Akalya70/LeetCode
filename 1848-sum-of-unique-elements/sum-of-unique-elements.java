31fcc5633c0c46a7464f0d7106d9daf6a1b08074
AKALYA <akalyamurugesan007@gmail.com>
2026-08-16 22:27:55 +0530

    Time: 2 ms (57.73%) | Memory: 43.1 MB (36.97%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+     int sum=0;
+    
+     HashMap<Integer,Integer>map=new HashMap<>();
+        for(int x:nums){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+    for(int x:map.keySet()){
+        if(map.get(x)==1){
+            sum+=x;
+        }
+    }return sum;
+     
+    }
+}
