ab514b1a056893547202d4eb6eddc448f4152c30
AKALYA <akalyamurugesan007@gmail.com>
2026-09-07 20:53:45 +0530

    Update unique-number-of-occurrences.java

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -3,7 +3,6 @@ class Solution {
         HashMap<Integer,Integer>map=new HashMap<>();
         for(int num:arr){
             map.put(num,map.getOrDefault(num,0)+1);
-
         }
         Set<Integer>set=new HashSet<>(map.values());
         return(map.size()==set.size());
