49aaad7131154f95ee3c805583e451dda5f0acde
AKALYA <akalyamurugesan007@gmail.com>
2026-09-07 20:56:41 +0530

    Fix loop structure in largestNumber method

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,7 +1,8 @@
 class Solution {
     public String largestNumber(int[] nums) {
         String str[]=new String[nums.length];
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++)
+        {
             str[i]=String.valueOf(nums[i]);
         }
         Arrays.sort(str,(a,b)->(b+a).compareTo(a+b));
