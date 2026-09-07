ade58d75e7326d111b20b43e4b42ccffb3cd6bd3
AKALYA <akalyamurugesan007@gmail.com>
2026-09-07 20:37:15 +0530

    Refactor day extraction in dayOfYear method

--- 1260-day-of-the-year/day-of-the-year.java
+++ 1260-day-of-the-year/day-of-the-year.java
@@ -2,11 +2,8 @@ class Solution {
     public int dayOfYear(String date) {
      int year = Integer.parseInt(date.substring(0, 4));
         int month = Integer.parseInt(date.substring(5, 7));
-        int day = Integer.parseInt(date.substring(8));
-        
+        int day = Integer.parseInt(date.substring(8)); 
         int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
-        
         if (isLeapYear(year))
          {
             daysInMonth[1] = 29; 
