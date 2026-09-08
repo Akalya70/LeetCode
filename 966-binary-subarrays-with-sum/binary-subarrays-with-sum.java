11a6dce00ec96cd2c19185f6a2f63152446fb962
AKALYA <akalyamurugesan007@gmail.com>
2026-09-08 20:10:14 +0530

    Fix formatting and add opening brace for method

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    public int numSubarraysWithSum(int[] nums, int goal) 
+    {
         int count=0;
         for(int i=0;i<nums.length;i++){
             int sum=0;
