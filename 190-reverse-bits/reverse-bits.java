af458af5a0672d0ccd1f8275a915ae53b5889a33
AKALYA <akalyamurugesan007@gmail.com>
2026-09-08 20:04:43 +0530

    Fix loop structure in reverseBits method

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,7 +1,8 @@
 class Solution {
     public int reverseBits(int n) {
         int result=0;
-        for(int i=0;i<32;i++){
+        for(int i=0;i<32;i++)
+        {
             result<<=1;
             result|=(n&1);
             n>>=1;
