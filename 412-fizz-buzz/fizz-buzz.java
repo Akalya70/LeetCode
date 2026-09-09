9a6fc36a03bbb01fd7952e2d598bbb0843d8168c
AKALYA <akalyamurugesan007@gmail.com>
2026-09-09 22:24:27 +0530

    Refactor fizzBuzz method formatting
    Fix formatting and add opening brace on a new line.

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,5 +1,6 @@
 class Solution {
-    public List<String> fizzBuzz(int n) {
+    public List<String> fizzBuzz(int n) 
+    {
         ArrayList<String>list=new ArrayList<>();
         for(int i=1;i<=n;i++){
             if(i%15==0){
