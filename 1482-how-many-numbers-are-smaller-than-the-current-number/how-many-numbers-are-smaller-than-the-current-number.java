c809b2c92e1b4045cd82819e275952b93707c8a3
AKALYA <akalyamurugesan007@gmail.com>
2026-09-09 22:15:01 +0530

    Fix formatting and add method body for smallerNumbersThanCurrent

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int[] smallerNumbersThanCurrent(int[] nums) {
+    public int[] smallerNumbersThanCurrent(int[] nums)
+    {
       int []ans=new int[nums.length];
      
       for(int i=0;i<nums.length;i++)  {
