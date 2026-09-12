a16b64b449afdfb0136425c3de129bc32ec141f1
AKALYA <akalyamurugesan007@gmail.com>
2026-09-12 23:07:06 +0530

    Correct method declaration and formatting
    Fix formatting and add opening brace for missingNumber method.

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumber(int[] nums)
+    {
        int sum=0;
        for(int i=0;i<nums.length;i++) {
         sum=sum+nums[i];
